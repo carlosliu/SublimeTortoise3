fdf9f38ad129456fd6d7a5fd5bef8ec12ccc496d
Fyneworks <dev@fyneworks.com>
2014-04-12 12:48:29 +0100

    added git bash shortcut a) ctrl+alt+v then ctrl+alt+b (to follow convention) b) ctrl+alt+b (shorter alternative, will work too)

--- Tortoise.py
+++ Tortoise.py
@@ -16,6 +16,52 @@ class NotFoundError(Exception):
 file_status_cache = {}
 
 
+# Borrowed from http://bit.ly/1iDl6f6
+def current_dir(window):
+    """
+    Return the working directory in which the window's commands should run.
+
+    In the common case when the user has one folder open, return that.
+    Otherwise, return one of the following (in order of preference):
+        1) One of the open folders, preferring a folder containing the active
+           file.
+        2) The directory containing the active file.
+        3) The user's home directory.
+    """
+    folders = window.folders()
+    if len(folders) == 1:
+        return folders[0]
+    else:
+        active_view = window.active_view()
+        active_file_name = active_view.file_name() if active_view else None
+        if not active_file_name:
+            return folders[0] if len(folders) else os.path.expanduser("~")
+        for folder in folders:
+            if active_file_name.startswith(folder):
+                return folder
+        return os.path.dirname(active_file_name)
+
+
+# Borrowed from TortoiseGIT package https://github.com/fyneworks/sublime-TortoiseGIT
+class TortoiseBashCommand(sublime_plugin.WindowCommand):
+    def run(self, paths=None, isHung=False):
+        dir = current_dir(self.window)
+
+        if not dir:
+            return
+            
+        settings = sublime.load_settings('Tortoise.sublime-settings')
+        git_bash_path = settings.get('git_bash_path')
+
+        if not os.path.isfile(git_bash_path):
+            sublime.error_message(''.join(['can\'t find sh.exe (gitbash),',
+                ' please config setting file', '\n   --sublime-TortoiseGIT']))
+            raise
+
+        command = 'cd %s & "%s" --login -i' % (dir, git_bash_path)
+        os.system(command)
+
+
 class TortoiseCommand():
     def get_path(self, paths):
         if paths == True:
